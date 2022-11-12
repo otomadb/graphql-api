aeec076ca4cb1b8a50f85af323f72b9103124281
SnO2WMaN <me@sno2wman.net>
2022-11-13 00:51:17 +0900

    video search

--- search.ts
+++ search.ts
@@ -1,8 +1,13 @@
 import { Database, ObjectId } from "mongo/mod.ts";
-import { getTagsCollection, Result } from "./get_video.ts";
+import { getTagsCollection, getVideosCollection, Result } from "./get_video.ts";
 
 export const search = async (db: Database, query: string): Promise<
   Result<{
+    videos: {
+      id: string;
+      title_search: string;
+      title_primary: string;
+    }[];
     tags: {
       id: string;
       name_search: string;
@@ -11,6 +16,35 @@ export const search = async (db: Database, query: string): Promise<
   }>
 > => {
   const tagsColl = getTagsCollection(db);
+  const videosColl = getVideosCollection(db);
+
+  const matchedVideos = await videosColl
+    .aggregate<{
+      id: string;
+      title_search: string;
+      title_primary: string;
+    }>([
+      { "$unwind": { path: "$titles" } },
+      { "$match": { "titles.title": { "$regex": query } } },
+      {
+        "$group": {
+          "_id": "$_id",
+          "id": { $first: "$id" },
+          "title_search": { $first: "$titles.title" },
+          "title_primary": { $first: "$title_primary" },
+        },
+      },
+      { "$sort": { "id": -1 } },
+      {
+        "$project": {
+          "_id": 0,
+          "id": 1,
+          "title_search": "$title_search",
+          "title_primary": "$title_primary",
+        },
+      },
+    ])
+    .toArray();
 
   const matchedTags = await tagsColl
     .aggregate<{
@@ -19,21 +53,32 @@ export const search = async (db: Database, query: string): Promise<
       name_primary: string;
     }>([
       { "$unwind": { path: "$names" } },
+      { "$match": { "names.name": { "$regex": query } } },
+      {
+        "$group": {
+          "_id": "$_id",
+          "id": { $first: "$id" },
+          "name_search": { $first: "$names.name" },
+          "name_primary": { $first: "$name_primary" },
+        },
+      },
+      { "$sort": { "id": -1 } },
       {
         "$project": {
           "_id": 0,
           "id": 1,
-          "name_search": "$names.name",
+          "name_search": "$name_search",
           "name_primary": "$name_primary",
         },
       },
-      { "$match": { "name_search": { "$regex": query } } },
-      { "$limit": 10 },
     ])
     .toArray();
 
   return {
     ok: true,
-    value: { tags: matchedTags },
+    value: {
+      videos: matchedVideos,
+      tags: matchedTags,
+    },
   };
 };
