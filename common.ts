2526f7842b98e4b3caa1f05d519c583c22008ead
SnO2WMaN <me@sno2wman.net>
2022-11-19 15:57:14 +0900

    remove unused

--- common.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLBoolean, GraphQLID, GraphQLNonNull, GraphQLString } from "graphql";
-
-export const GraphQLNonNullId = new GraphQLNonNull(GraphQLID);
-export const GraphQLNonNullString = new GraphQLNonNull(GraphQLString);
-export const GraphQLNonNullBoolean = new GraphQLNonNull(GraphQLBoolean);
--- result.ts
+++
@@ -1,3 +0,0 @@
-export type Result<V> =
-  | { ok: false; error: { status: number; message?: string } }
-  | { ok: true; value: V };
