1a80247d20902201c995353bd80f2ab04bef9538
Rokoucha <git@rokoucha.net>
2022-11-27 23:59:40 +0900

    src 配下に移動

--- tags/mod.ts
+++
@@ -1,4 +0,0 @@
-export { SearchTagsResultItem, Tag, TagName } from "./class.js";
-export { getTag } from "./get_tag.js";
-export { registerTag } from "./register_tag.js";
-export { searchTags } from "./search_tags.js";
