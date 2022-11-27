1e7259e748db275fb17770fc79dd7c0e40fd1d82
Rokoucha <git@rokoucha.net>
2022-11-28 02:23:58 +0900

    auth_old を削除

--- src/auth_old/mod.ts
+++
@@ -1,3 +0,0 @@
-export { refreshToken } from "./refresh_token.js";
-export { signin, SigninPayload } from "./signin.js";
-export { whoami } from "./whoami.js";
--- src/users/whoami.ts
+++ src/users/whoami.ts
@@ -1,6 +1,6 @@
 import { GraphQLError } from "graphql";
 import { MongoClient } from "mongodb";
-import { getUserById } from "../users/user.js";
+import { getUserById } from "./user.js";
 
 export const whoami = (_: unknown, { userId, mongo }: { userId: string; mongo: MongoClient }) => {
   if (!userId) throw new GraphQLError("Invalid access token!");
