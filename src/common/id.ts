5d8b59e0a4517e7fb69a1aa694870f5d1da96ce7
Rokoucha <git@rokoucha.net>
2022-12-03 21:27:52 +0900

    使わなくなったファイルを削除

--- src/common/id.ts
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const idGenerator = customAlphabet(
-  // abcdefghijklmnopqrstuvwxyz0123456789 - 0o1il
-  "abcdefghjkmnpqrstuvwxyz23456789",
-  12,
-);
-
-export const generateId = () => idGenerator();
--- src/videos/mod.ts
+++
@@ -1,8 +0,0 @@
-export { Video, VideoThumbnail, VideoTitle } from "./class.js";
-export { getVideo } from "./get_video.js";
-export { getVideos } from "./get_videos.js";
-export { VideoAddTagHistoryItem, VideoHistoryItem, VideoRegisterHistoryItem } from "./history_item_class.js";
-export { registerVideo } from "./register_video.js";
-export { searchVideos, SearchVideosResultItem } from "./search_videos.js";
-export { tagVideo } from "./tag_video.js";
-export { untagVideo } from "./untag_video.js";
