b3c990e8194a1c0ba90639549b23090bbe5c9a3f
SnO2WMaN <me@sno2wman.net>
2022-12-06 02:15:17 +0900

    no result

--- src/neo4j/register_video.ts
+++ src/neo4j/register_video.ts
@@ -1,12 +1,10 @@
 import { neo4jDriver } from "./driver.js";
 
-export const registerVideo = async (videoId: string, { tagIds }: { tagIds: string[] }): Promise<{ id: number }[]> => {
+export const registerVideo = async (videoId: string, { tagIds }: { tagIds: string[] }): Promise<void> => {
   const session = neo4jDriver.session();
 
   try {
-    console.dir(tagIds);
-
-    const result = await session.run(
+    await session.run(
       `
       MERGE (v:Video {id: $video_id})
       FOREACH (tag_id in $tag_ids | MERGE (t:Tag {id: tag_id}) MERGE (v)-[:TAGGED_BY]->(t))
@@ -17,7 +15,6 @@ export const registerVideo = async (videoId: string, { tagIds }: { tagIds: strin
         tag_ids: tagIds,
       }
     );
-    return result.records.map((rec) => ({ id: rec.get("id") }));
   } finally {
     await session.close();
   }
