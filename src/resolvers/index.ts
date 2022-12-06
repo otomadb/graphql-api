5747c70b80002f8ad4872c36e97a724c3bc0cc38
SnO₂WMaN <me@sno2wman.net>
2022-12-07 04:22:05 +0900

    fix: リソルバ周りを全て修正 (#44)
    Co-authored-by: rinsuki <428rinsuki+git@gmail.com>

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,25 +1,14 @@
 import { type Resolvers } from "../graphql/resolvers.js";
-import { registerTag, searchTags, tag, tags } from "./tags.js";
-import { user, whoami } from "./users.js";
-import { registerVideo, searchVideos, tagVideo, untagVideo, video, videos } from "./videos.js";
+import { resolveMutation } from "./mutation/index.js";
+import { resolveQuery } from "./query/index.js";
+import { resolveTag } from "./tag/index.js";
+import { resolveUser } from "./user/index.js";
+import { resolveVideo } from "./video/index.js";
 
 export const resolvers: Resolvers = {
-  Query: {
-    // findNiconicoSource // 最悪まだ実装しなくてもいい
-    // niconicoSource // 最悪まだ実装しなくてもいい
-    searchTags,
-    searchVideos,
-    tag,
-    tags,
-    user,
-    video,
-    videos,
-    whoami,
-  },
-  Mutation: {
-    registerTag,
-    registerVideo,
-    tagVideo,
-    untagVideo,
-  },
+  Query: resolveQuery,
+  Mutation: resolveMutation,
+  Tag: resolveTag,
+  Video: resolveVideo,
+  User: resolveUser,
 };
---
+++ src/resolvers/query/whoami.ts
@@ -0,0 +1,10 @@
+import { GraphQLError } from "graphql";
+
+import { UserModel } from "../../graphql/models.js";
+import { QueryResolvers } from "../../graphql/resolvers.js";
+
+export const whoami: QueryResolvers["whoami"] = async (_parent, _args, { user }) => {
+  if (!user) throw new GraphQLError("Invalid access token!");
+
+  return new UserModel(user);
+};
