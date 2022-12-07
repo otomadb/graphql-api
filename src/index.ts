e43f53715ed15fb06f9afa0d10a5ece21c687758
rinsuki <428rinsuki+git@gmail.com>
2022-12-07 15:08:52 +0900

    feat: replace with graphql-yoga server

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import "reflect-metadata";
 
 import { readFile } from "node:fs/promises";
 
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import koaCors from "@koa/cors";
 import Router from "@koa/router";
-import { graphql } from "graphql";
+import { createSchema, createYoga } from "graphql-yoga";
 import Koa from "koa";
 import { koaBody } from "koa-body";
 import logger from "koa-logger";
-import { z } from "zod";
 
 import { router as authRouter } from "./auth/index.js";
 import { getUserFromSession } from "./auth/session.js";
@@ -22,39 +20,31 @@ await dataSource.initialize();
 const app = new Koa();
 
 app.use(logger());
-app.use(koaBody());
+const koaBodyMiddleware = koaBody();
 app.use(koaCors({ credentials: true }));
 
 const router = new Router();
 
 export const typeDefs = await readFile(new URL("../schema.gql", import.meta.url), { encoding: "utf-8" });
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const schema = createSchema<Context>({ typeDefs, resolvers });
+const yoga = createYoga<Context>({ schema })
 
-router.use("/auth", authRouter.routes());
-router.use("/auth", authRouter.allowedMethods());
+router.use("/auth", koaBodyMiddleware, authRouter.routes(), authRouter.allowedMethods());
 
 router.post("/graphql", async (ctx) => {
   // まずは Cookie からセッションを取る、取れなければ Authorization ヘッダーから取る、形式は `Authorization: Bearer session_token`
   // FIXME: 危なそうなので開発環境だけ有効にしたい
   const user = await getUserFromSession(ctx.cookies.get("otmd-session") ?? ctx.get("authorization").split(" ").at(1));
-  const { query, variables, operationName } = z
-    .object({
-      query: z.string(),
-      variables: z.record(z.string(), z.any()).nullish(),
-      operationName: z.string().nullish(),
-    })
-    .parse(ctx.request.body);
-
   const contextValue: Context = { user };
 
-  ctx.body = await graphql({
-    contextValue,
-    operationName,
-    schema,
-    source: query,
-    variableValues: variables,
-  });
+  const response = await yoga.handleNodeRequest(ctx.req, contextValue);
+
+  ctx.status = response.status
+  for (const [key, val] of response.headers) {
+    ctx.set(key, val)
+  }
+  ctx.body = response.body
 
   return;
 });
