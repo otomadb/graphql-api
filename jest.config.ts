a5a8a16b50d96d4fa40006114884f764308acf13
SnO2WMaN <me@sno2wman.net>
2022-12-08 05:22:47 +0900

    more jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,14 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  setupFiles: ["<rootDir>/test/jest.setup.ts"],
+  testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+    // "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+    "^.+\\.tsx?$": ["@swc/jest", {}],
   },
 };
 
