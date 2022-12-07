5217d7cc826ee8524f4c5c3315481e104739e70e
SnO2WMaN <me@sno2wman.net>
2022-12-07 19:33:53 +0900

    vitestに置き換え

--- src/resolvers/user/index.test.ts
+++ src/resolvers/user/index.test.ts
@@ -1,8 +1,10 @@
+import { describe, expect, it, test } from "vitest";
+
 import { UserModel } from "../../graphql/models.js";
 import { resolveId } from "./index.js";
 
 describe("User resolver", () => {
-  describe("id", () => {
+  test("id", () => {
     it("add prefix", () => {
       const actual = resolveId({ id: "2" } as UserModel);
       expect(actual).toBe("user:2");
