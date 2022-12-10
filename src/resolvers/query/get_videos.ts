58d57d6bc5600cddccf1a53b3287313b65405046
SnO2WMaN <me@sno2wman.net>
2022-12-11 01:14:36 +0900

    inputの取り扱い方を修正

--- src/resolvers/query/get_videos.ts
+++ src/resolvers/query/get_videos.ts
@@ -8,11 +8,11 @@ export const getVideos =
   ({ dataSource }: { dataSource: DataSource }): QueryResolvers["videos"] =>
   async (_parent, { input }) => {
     const videos = await dataSource.getRepository(Video).find({
-      take: input?.limit || 0,
-      skip: input?.skip || 0,
+      take: input.limit || 0,
+      skip: input.skip || 0,
       order: {
-        createdAt: input?.order?.createdAt || undefined,
-        updatedAt: input?.order?.updatedAt || undefined,
+        createdAt: input.order?.createdAt || undefined,
+        updatedAt: input.order?.updatedAt || undefined,
       },
     });
 
