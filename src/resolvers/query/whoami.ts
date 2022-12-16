3b51bb21e532521a979820c4d8e7b80a6e5638b8
SnO₂WMaN <me@sno2wman.net>
2022-12-16 12:14:10 +0900

    feat: `Query.whoami`をnullableに (#96)

--- src/resolvers/query/whoami.ts
+++ src/resolvers/query/whoami.ts
@@ -1,12 +1,9 @@
-import { GraphQLError } from "graphql";
-
 import { UserModel } from "../../graphql/models.js";
 import { QueryResolvers } from "../../graphql/resolvers.js";
 
 export const whoami =
   (): QueryResolvers["whoami"] =>
   async (_parent, _args, { user }) => {
-    if (!user) throw new GraphQLError("Invalid access token!");
-
+    if (!user) return null;
     return new UserModel(user);
   };
