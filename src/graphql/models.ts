2669a409ce171737a754a2396e409b35175bb7f7
SnO₂WMaN <me@sno2wman.net>
2022-12-18 12:14:31 +0900

    feat: `Mylist.isLikeList`の実装 (#101)

--- src/graphql/models.ts
+++ src/graphql/models.ts
@@ -40,11 +40,13 @@ export class MylistModel {
   public id;
   public range;
   public title;
+  public isLikeList: boolean;
 
   constructor(private readonly mylist: Mylist) {
     this.id = mylist.id;
     this.range = mylist.range;
     this.title = mylist.title;
+    this.isLikeList = mylist.isLikeList;
   }
 }
 
