2afa3ab9b9c6a6af34d5234f3e2ac4849de3cd06
SnO₂WMaN <me@sno2wman.net>
2022-12-22 14:18:06 +0900

    refactor: `tagVideo`と`untagVideo`の名前変更 (#111)

--- src/resolvers/Mutation/index.ts
+++ src/resolvers/Mutation/index.ts
@@ -3,28 +3,28 @@ import { DataSource } from "typeorm";
 
 import { type Resolvers } from "../../graphql.js";
 import { addSemitagToVideo } from "./addSemitagToVideo.js";
+import { addTagToVideo } from "./addTagToVideo.js";
 import { addVideoToMylist } from "./addVideoToMylist.js";
 import { createMylist } from "./createMylist.js";
 import { likeVideo } from "./likeVideo.js";
 import { registerTag } from "./registerTag.js";
 import { registerVideo } from "./registerVideo.js";
+import { removeTagFromVideo } from "./removeTagFromVideo.js";
 import { removeVideoFromMylist } from "./removeVideoFromMylist.js";
 import { resolveSemitag } from "./resolveSemitag.js";
-import { tagVideo } from "./tagVideo.js";
 import { undoLikeVideo } from "./undoLikeVideo.js";
-import { untagVideo } from "./untagVideo.js";
 
 export const resolveMutation = (deps: { dataSource: DataSource; neo4jDriver: Neo4jDriver }) =>
   ({
+    addTagToVideo: addTagToVideo(deps),
     addSemitagToVideo: addSemitagToVideo(deps),
     addVideoToMylist: addVideoToMylist(deps),
     createMylist: createMylist(deps),
     likeVideo: likeVideo(deps),
     registerTag: registerTag(deps),
     registerVideo: registerVideo(deps),
+    removeTagFromVideo: removeTagFromVideo(deps),
     removeVideoFromMylist: removeVideoFromMylist(deps),
     resovleSemitag: resolveSemitag(deps),
-    tagVideo: tagVideo(deps),
     undoLikeVideo: undoLikeVideo(deps),
-    untagVideo: untagVideo(deps),
   } satisfies Resolvers["Mutation"]);
