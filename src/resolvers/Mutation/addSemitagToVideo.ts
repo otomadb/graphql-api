e9b9e0b23c632b75fb186b9b78fea1af39080845
SnO₂WMaN <me@sno2wman.net>
2022-12-28 03:48:47 +0900

    fix: `Mutation.addSemitagToVideo`でtransactionに纏める (#121)

--- src/resolvers/Mutation/addSemitagToVideo.ts
+++ src/resolvers/Mutation/addSemitagToVideo.ts
@@ -6,30 +6,32 @@ import { Semitag } from "../../db/entities/semitags.js";
 import { Video } from "../../db/entities/videos.js";
 import { MutationResolvers } from "../../graphql.js";
 import { parseGqlID } from "../../utils/id.js";
+import { SemitagModel } from "../Semitag/model.js";
 
-export const addSemitagToVideo = ({ dataSource: ds }: { dataSource: DataSource }) =>
+export const addSemitagToVideo = ({ dataSource }: { dataSource: DataSource }) =>
   (async (_parent, { input: { videoId: videoGqlId, name: semitagName } }) => {
     // TODO: auth
 
     const videoId = parseGqlID("video", videoGqlId);
     if (!videoId) throw new GraphQLError(`"${videoGqlId}" is invalid id for video`);
 
-    const video = await ds.getRepository(Video).findOne({ where: { id: videoId } });
-    if (!video) throw new GraphQLError(`No video found for "${videoGqlId}"`);
-
-    if (
-      await ds
-        .getRepository(Semitag)
-        .findOne({ where: { video: { id: video.id }, name: semitagName, resolved: false } })
-    )
-      throw new GraphQLError(`"${semitagName}" is already registered for "${videoGqlId}"`);
-
     const semitag = new Semitag();
     semitag.id = ulid();
     semitag.name = semitagName;
-    semitag.video = video;
 
-    await ds.getRepository(Semitag).insert(semitag);
+    await dataSource.transaction(async (manager) => {
+      const repoVideo = manager.getRepository(Video);
+      const repoSemitag = manager.getRepository(Semitag);
 
-    return { video, semitag };
+      const video = await repoVideo.findOne({ where: { id: videoId } });
+      if (!video) throw new GraphQLError(`No video found for "${videoGqlId}"`);
+
+      if (await repoSemitag.findOne({ where: { video: { id: video.id }, name: semitagName, resolved: false } }))
+        throw new GraphQLError(`"${semitagName}" is already registered for "${videoGqlId}"`);
+
+      semitag.video = video;
+      await repoSemitag.insert(semitag);
+    });
+
+    return { semitag: new SemitagModel(semitag) };
   }) satisfies MutationResolvers["addSemitagToVideo"];
