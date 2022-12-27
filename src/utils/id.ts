3bd2fd2b620042a4550e17c0fa5c105f1a3430d9
SnO2WMaN <me@sno2wman.net>
2022-12-27 23:11:03 +0900

    addTagToVideo

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -21,7 +21,7 @@ export function removeIDPrefix(type: ObjectType, id: string): string {
   return splitted[1];
 }
 
-type NodeType = "video" | "tag" | "semitag" | "nicovideoVideoSource";
+export type NodeType = "video" | "tag" | "semitag" | "nicovideoVideoSource";
 export const buildGqlId = (type: NodeType, dbId: string): string => `${type}:${dbId}`;
 export function parseGqlID(type: NodeType, gqlId: string): string | null {
   const separated = gqlId.split(":");
@@ -32,15 +32,23 @@ export function parseGqlID(type: NodeType, gqlId: string): string | null {
 
   return i;
 }
-export const buildInvalidGqlIdError = (type: NodeType, id: string) =>
-  new GraphQLError(`"${id}" is invalid id for "${type}"`);
+
+export function parseGqlID2(type: NodeType, gqlId: string): string {
+  const separated = gqlId.split(":");
+  if (separated.length !== 2) throw new GraphQLError(`"${gqlId}" is invalid id for "${type}"`);
+
+  const [t, i] = separated;
+  if (t !== type) throw new GraphQLError(`"${gqlId}" is invalid id for "${type}"`);
+
+  return i;
+}
 
 export function parseGqlIDs(type: NodeType, gqlIds: string[]): string[] {
-  const ids = [];
-  for (const exceptGqlId of gqlIds) {
-    const exceptId = parseGqlID(type, exceptGqlId);
-    if (!exceptId) throw buildInvalidGqlIdError(type, exceptGqlId);
-    ids.push(exceptId);
-  }
-  return ids;
+  return gqlIds.map((gqlId) => parseGqlID2(type, gqlId));
 }
+
+export const GraphQLInvalidIdError = (type: NodeType, invalidId: string) =>
+  new GraphQLError(`"${invalidId}" is invalid id for "${type}"`);
+
+export const GraphQLNotFoundError = (type: NodeType, gqlId: string) =>
+  new GraphQLError(`No "${type}" found for "${gqlId}"`);
