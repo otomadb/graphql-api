e397516d12086a0b8e9d8886007325ca208ea76a
SnO₂WMaN <me@sno2wman.net>
2022-12-31 10:15:20 +0900

    feat: `User.mylists`実装 (#129)

--- src/utils/id.ts
+++ src/utils/id.ts
@@ -21,7 +21,7 @@ export function removeIDPrefix(type: ObjectType, id: string): string {
   return splitted[1];
 }
 
-export type NodeType = "video" | "tag" | "semitag" | "nicovideoVideoSource" | "mylist";
+export type NodeType = "user" | "video" | "tag" | "semitag" | "nicovideoVideoSource" | "mylist";
 export const buildGqlId = (type: NodeType, dbId: string): string => `${type}:${dbId}`;
 
 export function parseGqlID(type: NodeType, gqlId: string): string {
