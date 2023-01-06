d6b38a775ad40bc3e4770f159869ac7a4b09a900
SnO2WMaN <me@sno2wman.net>
2023-01-06 10:49:02 +0900

    rm removeMylistRegistration

--- src/resolvers/Mutation/undoLikeVideo/undoLikeVideo.ts
+++ src/resolvers/Mutation/undoLikeVideo/undoLikeVideo.ts
@@ -5,10 +5,29 @@ import { DataSource } from "typeorm";
 import { MylistRegistration } from "../../../db/entities/mylist_registrations.js";
 import { Mylist } from "../../../db/entities/mylists.js";
 import { MutationResolvers } from "../../../graphql.js";
-import { removeMylistRegistration as removeMylistRegistrationInNeo4j } from "../../../neo4j/removeMylistRegistration.js";
 import { parseGqlID } from "../../../utils/id.js";
 import { MylistModel } from "../../Mylist/model.js";
 import { VideoModel } from "../../Video/model.js";
+
+export const undoLikeVideoInNeo4j = async (
+  neo4jDriver: Neo4jDriver,
+  { mylistId, videoId }: { mylistId: string; videoId: string }
+) => {
+  const session = neo4jDriver.session();
+  try {
+    await session.run(
+      `
+        MATCH (l:Mylist {id: $mylist_id })
+        MATCH (v:Video {id: $video_id })
+        MATCH (l)-[r:CONTAINS_VIDEO]->(v)
+        DELETE r
+        `,
+      { mylist_id: mylistId, video_id: videoId }
+    );
+  } finally {
+    await session.close();
+  }
+};
 
 export const undoLikeVideo = ({ dataSource: ds, neo4jDriver }: { dataSource: DataSource; neo4jDriver: Neo4jDriver }) =>
   (async (_, { input: { videoId: videoGqlId } }, { user }) => {
@@ -33,7 +52,10 @@ export const undoLikeVideo = ({ dataSource: ds, neo4jDriver }: { dataSource: Dat
 
     await repoMylistRegistration.remove(registration);
 
-    await removeMylistRegistrationInNeo4j({ neo4jDriver })(registration);
+    await undoLikeVideoInNeo4j(neo4jDriver, {
+      mylistId: registration.mylist.id,
+      videoId: registration.video.id,
+    });
 
     return {
       video: new VideoModel(registration.video),
