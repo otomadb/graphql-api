4b15fcfac5d1ea34397945c4035b87e93f8f0d47
SnO₂WMaN <me@sno2wman.net>
2023-01-24 19:32:21 +0900

    feat: 管理者が存在しない場合は自動的に管理者としてsignupする (#165)

--- src/auth/signup.ts
+++ src/auth/signup.ts
@@ -6,76 +6,84 @@ import { z } from "zod";
 
 import { Mylist, MylistShareRange } from "../db/entities/mylists.js";
 import { User, UserRole } from "../db/entities/users.js";
+import { Result } from "../utils/Result.js";
 import { createSession } from "./createSession.js";
+
+const reqBodySchema = z.object({
+  name: z.string(),
+  displayName: z.string(),
+  email: z.string(),
+  password: z.string(),
+});
+
+export const registerUser = async (
+  dataSource: DataSource,
+  input: z.infer<typeof reqBodySchema>
+): Promise<Result<{ message: "USER_NAME_ALREADY_REGISTERED" }, { user: User }>> => {
+  const { name, displayName, email, password: rawPassword } = input;
+
+  if (await dataSource.getRepository(User).findOneBy({ name }))
+    return {
+      status: "error",
+      error: { message: "USER_NAME_ALREADY_REGISTERED" },
+    };
+
+  const user = new User();
+  user.id = ulid();
+  user.name = name;
+  user.displayName = displayName;
+  user.email = email;
+  user.password = await argon2.hash(rawPassword, {
+    type: 2,
+    memoryCost: 15 * 1024,
+    timeCost: 2,
+    parallelism: 1,
+  });
+  user.icon = null;
+  user.emailConfirmed = true; // FIXME: あとでなおす
+  if (await dataSource.getRepository(User).findOneBy({ role: UserRole.ADMINISTRATOR })) user.role = UserRole.NORMAL;
+  else user.role = UserRole.ADMINISTRATOR;
+
+  // Add user likes
+  const userLikes = new Mylist();
+  userLikes.id = ulid();
+  userLikes.isLikeList = true;
+  userLikes.title = `favorites for ${user.displayName}`;
+  userLikes.range = MylistShareRange.PRIVATE;
+  userLikes.holder = user;
+
+  await dataSource.transaction(async (e) => {
+    await e.getRepository(User).insert(user);
+    await e.getRepository(Mylist).insert(userLikes);
+  });
+
+  return { status: "ok", data: { user } };
+};
 
 export const handlerSignup = ({ dataSource }: { dataSource: DataSource }) =>
   (async (req, reply) => {
-    const parseRequest = z
-      .object({ name: z.string(), displayName: z.string(), email: z.string(), password: z.string() })
-      .safeParse(req.body);
+    const parsedReqBody = reqBodySchema.safeParse(req.body);
 
-    if (!parseRequest.success) {
-      reply.status(400);
-      reply.send({ code: "INVALID_REQUEST" });
-      return;
+    if (!parsedReqBody.success) return reply.status(400).send({ code: "INVALID_REQUEST" });
+
+    const result = await registerUser(dataSource, parsedReqBody.data);
+    if (result.status === "error") {
+      switch (result.error.message) {
+        case "USER_NAME_ALREADY_REGISTERED":
+          return reply.status(400).send({ code: "USER_NAME_ALREADY_REGISTERED" });
+      }
     }
-    const { name, displayName, email, password } = parseRequest.data;
-
-    if (
-      await dataSource
-        .getRepository(User)
-        .findOne({ where: { name } })
-        .then((v) => !!v)
-    ) {
-      reply.status(400);
-      reply.send({ code: "USER_NAME_ALREADY_REGISTERED" });
-      return;
-    }
-
-    if (
-      await dataSource
-        .getRepository(User)
-        .findOne({ where: { email } })
-        .then((v) => !!v)
-    ) {
-      reply.status(400);
-      reply.send({ code: "EMAIL_ALREADY_REGISTERED" });
-      return;
-    }
-
-    const passwordHash = await argon2.hash(password, {
-      type: 2,
-      memoryCost: 15 * 1024,
-      timeCost: 2,
-      parallelism: 1,
-    });
-    const user = new User();
-    user.id = ulid();
-    user.name = name;
-    user.displayName = displayName;
-    user.email = email;
-    user.icon = null;
-    user.emailConfirmed = true; // FIXME: あとでなおす
-    user.password = passwordHash;
-    user.role = UserRole.NORMAL;
-
-    const userRepository = dataSource.getRepository(User);
-    await userRepository.insert(user);
-
-    await dataSource.getRepository(Mylist).insert({
-      id: ulid(),
-      title: `favorites for ${user.displayName}`,
-      range: MylistShareRange.PRIVATE,
-      holder: { id: user.id },
-      isLikeList: true,
-    });
+    const {
+      data: { user },
+    } = result;
 
     const session = await createSession(dataSource, user);
-    reply.setCookie("otmd-session", session, {
-      httpOnly: true,
-      secure: "auto",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      path: "/",
-    });
-    reply.send({ id: user.id });
+    return reply
+      .setCookie("otmd-session", session, {
+        httpOnly: true,
+        secure: "auto",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        path: "/",
+      })
+      .send({ id: user.id });
   }) satisfies RouteHandlerMethod;
