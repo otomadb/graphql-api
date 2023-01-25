5df3de80244ce4015103489d398ff0471357afd8
SnO2WMaN <me@sno2wman.net>
2023-01-25 20:57:14 +0900

    Tag

--- src/resolvers/Tag/model.ts
+++ src/resolvers/Tag/model.ts
@@ -1,8 +1,10 @@
+import { Tag } from "@prisma/client";
+
 export class TagModel {
   public id;
   public meaningless;
 
-  constructor(tag: { id: string; meaningless: boolean }) {
+  constructor(tag: Tag) {
     this.id = tag.id;
     this.meaningless = tag.meaningless;
   }
