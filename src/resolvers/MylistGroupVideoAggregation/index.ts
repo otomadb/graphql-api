81109174876988a6914027f6195b95a87f60c20c
SnO2WMaN <me@sno2wman.net>
2023-01-25 22:24:48 +0900

    MylistGroupVideoAggregation

--- src/resolvers/MylistGroupVideoAggregation/index.ts
+++ src/resolvers/MylistGroupVideoAggregation/index.ts
@@ -1,30 +1,29 @@
-import { DataSource, In } from "typeorm";
-
-import { Mylist } from "../../db/entities/mylists.js";
-import { Video } from "../../db/entities/videos.js";
 import { Resolvers } from "../../graphql.js";
 import { GraphQLNotExistsInDBError } from "../../utils/id.js";
+import { ResolverDeps } from "../index.js";
 import { MylistModel } from "../Mylist/model.js";
 import { VideoModel } from "../Video/model.js";
 
-export const resolveMylistGroupVideoAggregation = ({ dataSource }: { dataSource: DataSource }) =>
+export const resolveMylistGroupVideoAggregation = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
   ({
     video: ({ videoId }) =>
-      dataSource
-        .getRepository(Video)
-        .findOneOrFail({ where: { id: videoId } })
+      prisma.video
+        .findUniqueOrThrow({ where: { id: videoId } })
         .then((v) => new VideoModel(v))
         .catch(() => {
           throw new GraphQLNotExistsInDBError("Video", videoId);
         }),
     mylists: ({ mylistIds }, { input }) =>
-      dataSource
-        .getRepository(Mylist)
-        .find({
-          where: { id: In(mylistIds) },
-          order: {
+      prisma.mylist
+        .findMany({
+          where: { id: { in: mylistIds } },
+          orderBy: {
+            // TODO: Prisma
+            createdAt: "asc",
+            /*
             createdAt: input.order.createdAt || undefined,
             updatedAt: input.order.updatedAt || undefined,
+  */
           },
           take: input.limit,
           skip: input.skip,
