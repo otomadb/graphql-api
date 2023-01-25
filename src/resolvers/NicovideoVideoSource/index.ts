2986b177ee511154e273b05c9ba659582db5be0a
SnO2WMaN <me@sno2wman.net>
2023-01-25 22:19:13 +0900

    NicovideoSource

--- src/resolvers/NicovideoVideoSource/index.ts
+++ src/resolvers/NicovideoVideoSource/index.ts
@@ -1,18 +1,17 @@
-import { GraphQLError } from "graphql";
-import { DataSource } from "typeorm";
-
-import { Video } from "../../db/entities/videos.js";
 import { Resolvers } from "../../graphql.js";
-import { buildGqlId } from "../../utils/id.js";
+import { buildGqlId, GraphQLNotExistsInDBError } from "../../utils/id.js";
+import { ResolverDeps } from "../index.js";
 import { VideoModel } from "../Video/model.js";
 
-export const resolveNicovideoVideoSource = ({ dataSource }: { dataSource: DataSource }) =>
+export const resolveNicovideoVideoSource = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
   ({
     id: ({ id }) => buildGqlId("NicovideoVideoSource", id),
     url: ({ sourceId }) => `https://www.nicovideo.jp/watch/${sourceId}`,
-    video: async ({ videoId }) => {
-      const video = await dataSource.getRepository(Video).findOne({ where: { id: videoId } });
-      if (!video) throw new GraphQLError(`Video cannot find with id "${videoId}"`);
-      return new VideoModel(video);
-    },
+    video: async ({ videoId }) =>
+      prisma.video
+        .findUniqueOrThrow({ where: { id: videoId } })
+        .then((v) => new VideoModel(v))
+        .catch(() => {
+          throw new GraphQLNotExistsInDBError("Video", videoId);
+        }),
   } satisfies Resolvers["NicovideoVideoSource"]);
