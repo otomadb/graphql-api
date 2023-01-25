52f59c6c57e4c4a42f0c0530c157418056b71cb1
SnO2WMaN <me@sno2wman.net>
2023-01-26 04:12:15 +0900

    searchTags & searchVideos

--- src/resolvers/Query/searchTags/searchTags.ts
+++ src/resolvers/Query/searchTags/searchTags.ts
@@ -1,32 +1,21 @@
-import { GraphQLError } from "graphql";
-
 import { QueryResolvers } from "../../../graphql.js";
 import { ResolverDeps } from "../../index.js";
 import { TagModel } from "../../Tag/model.js";
 
 export const searchTags = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
   (async (_, { input }) => {
-    const tagNames = await dataSource
-      .getRepository(TagName)
-      .createQueryBuilder("tagName")
-      .where({ name: Like(`%${input.query}%`) })
-      .leftJoinAndSelect("tagName.tag", "tags")
-      .distinctOn(["tagName.tag.id"])
-      .skip(input.skip)
-      .limit(input.limit)
-      .getMany();
-
-    const tags = await dataSource.getRepository(Tag).find({
-      where: { id: In(tagNames.map((t) => t.tag.id)) },
+    const tags = await prisma.tagName.findMany({
+      where: { name: { contains: input.query } },
+      distinct: "tagId",
+      include: { tag: true },
+      skip: input.skip || undefined,
+      take: input.limit || undefined,
     });
-
     return {
-      items: tagNames.map((n) => {
-        const tag = tags.find((t) => t.id === n.tag.id);
-        if (!tag) throw new GraphQLError(`Data inconcistency is occuring for "video:${n.tag.id}"`);
+      items: tags.map((t) => {
         return {
-          matchedName: n.name,
-          tag: new TagModel(tag),
+          matchedName: t.name,
+          tag: new TagModel(t.tag),
         };
       }),
     };
