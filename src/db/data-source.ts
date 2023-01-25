940987fcbdfd2fc637a16bffcf00f497e74d675d
SnO2WMaN <me@sno2wman.net>
2023-01-25 11:33:07 +0900

    fix: Migrationの実行時型エラーを無理やり修正

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -5,11 +5,13 @@ import { entities } from "./entities/index.js";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  /* eslint-disable @typescript-eslint/no-non-null-assertion */
+  host: process.env.POSTGRES_HOST!,
+  port: parseInt(process.env.POSTGRES_PORT!, 10),
+  username: process.env.POSTGRES_USERNAME!,
+  password: process.env.POSTGRES_PASSWORD!,
+  database: process.env.POSTGRES_DATABASE!,
+  /* eslint-enable @typescript-eslint/no-non-null-assertion */
   entities,
   migrations: [`${dirname(new URL(import.meta.url).pathname)}/migrations/*.ts`],
 });
