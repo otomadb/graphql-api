9d80c9851040c1870d234338f31abfc130685175
SnO2WMaN <me@sno2wman.net>
2023-01-25 11:14:14 +0900

    fix: migrationの環境変数周り修正

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -5,7 +5,11 @@ import { entities } from "./entities/index.js";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities,
   migrations: [`${dirname(new URL(import.meta.url).pathname)}/migrations/*.ts`],
 });
