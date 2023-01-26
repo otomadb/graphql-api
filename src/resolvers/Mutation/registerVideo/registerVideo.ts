1255b0c5d20290d686589b3c49d3a820cfaaf2fe
SnO2WMaN <me@sno2wman.net>
2023-01-26 17:50:48 +0900

    registerVideo

--- src/resolvers/Mutation/registerVideo/registerVideo.ts
+++ src/resolvers/Mutation/registerVideo/registerVideo.ts
@@ -83,6 +83,7 @@ export const registerVideoScaffold =
           createMany: {
             data: input.semitags.map((name) => ({
               name,
+              isResolved: false,
             })),
           },
         },
