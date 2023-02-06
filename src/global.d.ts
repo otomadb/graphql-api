caa324b1b299b5396518af1524c98f13bb9cfb3b
SnO₂WMaN <me@sno2wman.net>
2023-02-06 10:23:44 +0900

    fix: 環境変数の取り扱いを変更 (#193)

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,16 +2,9 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly HOST?: string;
-    readonly PORT?: string;
-
     readonly NODE_ENV: "development" | "production";
 
-    readonly POSTGRES_HOST: string;
-    readonly POSTGRES_PORT: string;
-    readonly POSTGRES_USERNAME: string;
-    readonly POSTGRES_PASSWORD: string;
-    readonly POSTGRES_DATABASE: string;
+    readonly PRISMA_DATABASE_URL: string;
 
     readonly NEO4J_URL: string;
     readonly NEO4J_USERNAME: string;
