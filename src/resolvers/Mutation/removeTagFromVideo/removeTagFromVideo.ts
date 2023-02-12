60a4416adbbf445645289e1d29792eb144202a63
SnO2WMaN <me@sno2wman.net>
2023-02-12 10:04:53 +0900

    feat: `Mutation.removeTagFromVideo`のResultUnion化
    Fixes #256

--- src/resolvers/Mutation/removeTagFromVideo/removeTagFromVideo.ts
+++ src/resolvers/Mutation/removeTagFromVideo/removeTagFromVideo.ts
@@ -1,11 +1,9 @@
 import { Tag, UserRole, Video, VideoTag } from "@prisma/client";
-import { GraphQLError } from "graphql";
 import { Driver as Neo4jDriver } from "neo4j-driver";
 
 import { Result } from "../../../utils/Result.js";
-import { ensureContextUser } from "../../ensureContextUser.js";
-import { MutationResolvers } from "../../graphql.js";
-import { GraphQLNotExistsInDBError, parseGqlID } from "../../id.js";
+import { MutationResolvers, RemoveTagFromVideoFailedMessage } from "../../graphql.js";
+import { parseGqlID2 } from "../../id.js";
 import { ResolverDeps } from "../../index.js";
 import { TagModel } from "../../Tag/model.js";
 import { VideoModel } from "../../Video/model.js";
@@ -59,30 +57,63 @@ export const remove = async (
 };
 
 export const removeTagFromVideo = ({ prisma, neo4j }: Pick<ResolverDeps, "prisma" | "neo4j">) =>
-  ensureContextUser(
-    prisma,
-    UserRole.NORMAL,
-    async (_parent, { input: { tagId: tagGqlId, videoId: videoGqlId } }, { userId: authUserId }) => {
-      const videoId = parseGqlID("Video", videoGqlId);
-      const tagId = parseGqlID("Tag", tagGqlId);
+  (async (_parent, { input: { tagId: tagGqlId, videoId: videoGqlId } }, { user }) => {
+    if (!user || (user?.role !== UserRole.EDITOR && user?.role !== UserRole.ADMINISTRATOR))
+      return {
+        __typename: "RemoveTagFromVideoFailedPayload",
+        message: RemoveTagFromVideoFailedMessage.Forbidden,
+      };
 
-      const result = await remove(prisma, { videoId, tagId, authUserId });
-      if (result.status === "error") {
-        switch (result.error) {
-          case "NO_VIDEO":
-            throw new GraphQLNotExistsInDBError("Video", videoId);
-          case "NO_TAG":
-            throw new GraphQLNotExistsInDBError("Video", videoId);
-          case "NO_TAGGING":
-            throw new GraphQLError("Tagging does not exist");
-          case "REMOVED_TAGGING":
-            throw new GraphQLError("Tagging is removed already");
-        }
+    const videoId = parseGqlID2("Video", videoGqlId);
+    if (videoId.status === "error")
+      return {
+        __typename: "RemoveTagFromVideoFailedPayload",
+        message: RemoveTagFromVideoFailedMessage.InvalidVideoId,
+      };
+
+    const tagId = parseGqlID2("Tag", tagGqlId);
+    if (tagId.status === "error")
+      return {
+        __typename: "RemoveTagFromVideoFailedPayload",
+        message: RemoveTagFromVideoFailedMessage.InvalidTagId,
+      };
+
+    const result = await remove(prisma, {
+      videoId: videoId.data,
+      tagId: tagId.data,
+      authUserId: user.id,
+    });
+    if (result.status === "error") {
+      switch (result.error) {
+        case "NO_VIDEO":
+          return {
+            __typename: "RemoveTagFromVideoFailedPayload",
+            message: RemoveTagFromVideoFailedMessage.VideoNotFound,
+          };
+        case "NO_TAG":
+          return {
+            __typename: "RemoveTagFromVideoFailedPayload",
+            message: RemoveTagFromVideoFailedMessage.TagNotFound,
+          };
+        case "NO_TAGGING":
+          return {
+            __typename: "RemoveTagFromVideoFailedPayload",
+            message: RemoveTagFromVideoFailedMessage.NoTagging,
+          };
+        case "REMOVED_TAGGING":
+          return {
+            __typename: "RemoveTagFromVideoFailedPayload",
+            message: RemoveTagFromVideoFailedMessage.TaggingAlreadyRemoved,
+          };
       }
-
-      const tagging = result.data;
-      await removeInNeo4j(neo4j, { videoId: tagging.video.id, tagId: tagging.tag.id });
-
-      return { video: new VideoModel(tagging.video), tag: new TagModel(tagging.tag) };
     }
-  ) satisfies MutationResolvers["removeTagFromVideo"];
+
+    const tagging = result.data;
+    await removeInNeo4j(neo4j, { videoId: tagging.video.id, tagId: tagging.tag.id });
+
+    return {
+      __typename: "RemoveTagFromVideoSuccessedPayload",
+      tag: new TagModel(tagging.tag),
+      video: new VideoModel(tagging.video),
+    };
+  }) satisfies MutationResolvers["removeTagFromVideo"];
