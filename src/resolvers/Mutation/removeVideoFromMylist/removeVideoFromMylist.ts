318707c7e32df7ac8680063fb1c968d3cbf40f65
SnO2WMaN <me@sno2wman.net>
2023-02-13 07:25:42 +0900

    feat: RemoveVideoFromMylistでdeleteするのを止める
    Fixes #297

--- src/resolvers/Mutation/removeVideoFromMylist/removeVideoFromMylist.ts
+++ src/resolvers/Mutation/removeVideoFromMylist/removeVideoFromMylist.ts
@@ -1,10 +1,9 @@
-import { UserRole } from "@prisma/client";
-import { GraphQLError } from "graphql";
+import { Mylist, MylistRegistration, Video } from "@prisma/client";
 import { Driver as Neo4jDriver } from "neo4j-driver";
 
-import { ensureContextUser } from "../../ensureContextUser.js";
-import { MutationResolvers } from "../../graphql.js";
-import { parseGqlID } from "../../id.js";
+import { err, ok, Result } from "../../../utils/Result.js";
+import { MutationResolvers, RemoveVideoFromMylistFailedMessage } from "../../graphql.js";
+import { parseGqlID2 } from "../../id.js";
 import { ResolverDeps } from "../../index.js";
 import { MylistModel } from "../../Mylist/model.js";
 import { VideoModel } from "../../Video/model.js";
@@ -29,30 +28,102 @@ export const removeMylistRegistrationInNeo4j = async (
   }
 };
 
+export const remove = async (
+  prisma: ResolverDeps["prisma"],
+  { videoId, mylistId, userId }: { userId: string; videoId: string; mylistId: string }
+): Promise<
+  Result<
+    "VIDEO_NOT_FOUND" | "MYLIST_NOT_FOUND" | "NOT_MYLIST_HOLDER" | "NOT_REGISTERED" | "ALREADY_UNREGISTERED",
+    MylistRegistration & { mylist: Mylist; video: Video }
+  >
+> => {
+  const video = await prisma.video.findUnique({ where: { id: videoId } });
+  if (!video) return err("VIDEO_NOT_FOUND");
+
+  const mylist = await prisma.mylist.findFirst({ where: { id: mylistId } });
+  if (!mylist) return err("MYLIST_NOT_FOUND");
+  if (mylist.holderId !== userId) return err("NOT_MYLIST_HOLDER");
+
+  const ext = await prisma.mylistRegistration.findUnique({
+    where: { mylistId_videoId: { mylistId: mylist.id, videoId: video.id } },
+  });
+  if (!ext) return err("NOT_REGISTERED");
+  if (!ext.isRemoved) return err("ALREADY_UNREGISTERED");
+
+  const registration = await prisma.mylistRegistration.update({
+    where: { id: ext.id },
+    data: { isRemoved: true, events: { create: { type: "UNREGISTER", userId, payload: {} } } },
+    include: { video: true, mylist: true },
+  });
+  return ok(registration);
+};
+
 export const removeVideoFromMylist = ({ prisma, neo4j }: Pick<ResolverDeps, "prisma" | "neo4j">) =>
-  ensureContextUser(
-    prisma,
-    UserRole.NORMAL,
-    async (_, { input: { mylistId: mylistGqlId, videoId: videoGqlId } }, { userId }) => {
-      const videoId = parseGqlID("Video", videoGqlId);
-      const mylistId = parseGqlID("Mylist", mylistGqlId);
-
-      if ((await prisma.mylist.findUniqueOrThrow({ where: { id: mylistId } })).holderId !== userId)
-        throw new GraphQLError("This mylist is not yours");
-
-      const registration = await prisma.mylistRegistration.delete({
-        where: { mylistId_videoId: { videoId, mylistId } },
-        include: { video: true, mylist: true },
-      });
-
-      await removeMylistRegistrationInNeo4j(neo4j, {
-        mylistId: registration.mylist.id,
-        videoId: registration.video.id,
-      });
-
+  (async (_, { input: { mylistId: mylistGqlId, videoId: videoGqlId } }, { user }) => {
+    if (!user)
       return {
-        video: new VideoModel(registration.video),
-        mylist: new MylistModel(registration.mylist),
+        __typename: "RemoveVideoFromMylistFailedPayload",
+        message: RemoveVideoFromMylistFailedMessage.Forbidden,
       };
+
+    const videoId = parseGqlID2("Video", videoGqlId);
+    if (videoId.status === "error")
+      return {
+        __typename: "RemoveVideoFromMylistFailedPayload",
+        message: RemoveVideoFromMylistFailedMessage.InvalidVideoId,
+      };
+
+    const mylistId = parseGqlID2("Mylist", mylistGqlId);
+    if (mylistId.status === "error")
+      return {
+        __typename: "RemoveVideoFromMylistFailedPayload",
+        message: RemoveVideoFromMylistFailedMessage.InvalidMylistId,
+      };
+
+    const result = await remove(prisma, {
+      userId: user.id,
+      videoId: videoId.data,
+      mylistId: mylistId.data,
+    });
+    if (result.status === "error") {
+      switch (result.error) {
+        case "VIDEO_NOT_FOUND":
+          return {
+            __typename: "RemoveVideoFromMylistFailedPayload",
+            message: RemoveVideoFromMylistFailedMessage.VideoNotFound,
+          };
+        case "MYLIST_NOT_FOUND":
+          return {
+            __typename: "RemoveVideoFromMylistFailedPayload",
+            message: RemoveVideoFromMylistFailedMessage.MylistNotFound,
+          };
+        case "NOT_MYLIST_HOLDER":
+          return {
+            __typename: "RemoveVideoFromMylistFailedPayload",
+            message: RemoveVideoFromMylistFailedMessage.WrongMylistHolder,
+          };
+        case "NOT_REGISTERED":
+          return {
+            __typename: "RemoveVideoFromMylistFailedPayload",
+            message: RemoveVideoFromMylistFailedMessage.NotRegistered,
+          };
+        case "ALREADY_UNREGISTERED":
+          return {
+            __typename: "RemoveVideoFromMylistFailedPayload",
+            message: RemoveVideoFromMylistFailedMessage.AlreadyUnregistered,
+          };
+      }
     }
-  ) satisfies MutationResolvers["removeVideoFromMylist"];
+
+    const registration = result.data;
+    await removeMylistRegistrationInNeo4j(neo4j, {
+      mylistId: registration.mylist.id,
+      videoId: registration.video.id,
+    });
+
+    return {
+      __typename: "RemoveVideoFromMylistSucceededPayload",
+      video: new VideoModel(registration.video),
+      mylist: new MylistModel(registration.mylist),
+    };
+  }) satisfies MutationResolvers["removeVideoFromMylist"];
