0a0bddba64955c64686aa3576dbb8b111c66e86c
SnO2WMaN <me@sno2wman.net>
2023-02-15 19:17:25 +0900

    test: `User.likes`のテスト書き直し

---
+++ src/resolvers/User/likes.e2e.test.ts
@@ -0,0 +1,219 @@
+import { describe, expect } from "@jest/globals";
+import { MylistShareRange, PrismaClient } from "@prisma/client";
+
+import { cleanPrisma } from "../../test/cleanPrisma.js";
+import { Err, err, Ok, ok } from "../../utils/Result.js";
+import { ResolverDeps } from "../index.js";
+import { get } from "./likes.js";
+
+describe("User.likes", () => {
+  let prisma: ResolverDeps["prisma"];
+
+  beforeAll(async () => {
+    prisma = new PrismaClient({ datasources: { db: { url: process.env.TEST_PRISMA_DATABASE_URL } } });
+    await prisma.$connect();
+  });
+
+  beforeEach(async () => {
+    await cleanPrisma(prisma);
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  test("いいね欄はPRIVATEで，認証していない", async () => {
+    await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          id: "u1",
+          name: "user1",
+          displayName: "User1",
+          email: "user1@example.com",
+          password: "password",
+        },
+      }),
+      prisma.mylist.create({
+        data: {
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.PRIVATE,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: null });
+    expect(actual).toStrictEqual(err("PRIVATE_NOT_HOLDER") satisfies Err<Awaited<ReturnType<typeof get>>>);
+  });
+
+  test("いいね欄はPRIVATEで，認証しているが，いいね欄の持ち主ではない", async () => {
+    await prisma.$transaction([
+      prisma.user.createMany({
+        data: [
+          {
+            id: "u1",
+            name: "user1",
+            displayName: "User1",
+            email: "user1@example.com",
+            password: "password",
+          },
+          {
+            id: "u2",
+            name: "user2",
+            displayName: "User2",
+            email: "user2@example.com",
+            password: "password",
+          },
+        ],
+      }),
+      prisma.mylist.create({
+        data: {
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.PRIVATE,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: "u2" });
+    expect(actual).toStrictEqual(err("PRIVATE_NOT_HOLDER") satisfies Err<Awaited<ReturnType<typeof get>>>);
+  });
+
+  test("いいね欄はKNOW_LINKで，認証していない", async () => {
+    await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          id: "u1",
+          name: "user1",
+          displayName: "User1",
+          email: "user1@example.com",
+          password: "password",
+        },
+      }),
+      prisma.mylist.create({
+        data: {
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.PRIVATE,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: null });
+    expect(actual).toStrictEqual(err("PRIVATE_NOT_HOLDER") satisfies Err<Awaited<ReturnType<typeof get>>>);
+  });
+
+  test("いいね欄はKNOW_LINKで，認証しているが，いいね欄の持ち主ではない", async () => {
+    await prisma.$transaction([
+      prisma.user.createMany({
+        data: [
+          {
+            id: "u1",
+            name: "user1",
+            displayName: "User1",
+            email: "user1@example.com",
+            password: "password",
+          },
+          {
+            id: "u2",
+            name: "user2",
+            displayName: "User2",
+            email: "user2@example.com",
+            password: "password",
+          },
+        ],
+      }),
+      prisma.mylist.create({
+        data: {
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.KNOW_LINK,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: "u2" });
+    expect(actual).toStrictEqual(err("PRIVATE_NOT_HOLDER") satisfies Err<Awaited<ReturnType<typeof get>>>);
+  });
+
+  test("いいね欄はPUBLIC", async () => {
+    await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          id: "u1",
+          name: "user1",
+          displayName: "User1",
+          email: "user1@example.com",
+          password: "password",
+        },
+      }),
+      prisma.mylist.create({
+        data: {
+          id: "m1",
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.PUBLIC,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: null });
+    expect(actual).toStrictEqual(
+      ok(
+        expect.objectContaining({
+          id: "m1",
+          title: "Mylist 1",
+          holderId: "u1",
+        } satisfies Partial<Ok<Awaited<ReturnType<typeof get>>>["data"]>)
+      )
+    );
+  });
+
+  test("いいね欄がPUBLICなら，認証したユーザが違っても取得可能", async () => {
+    await prisma.$transaction([
+      prisma.user.createMany({
+        data: [
+          {
+            id: "u1",
+            name: "user1",
+            displayName: "User1",
+            email: "user1@example.com",
+            password: "password",
+          },
+          {
+            id: "u2",
+            name: "user2",
+            displayName: "User2",
+            email: "user2@example.com",
+            password: "password",
+          },
+        ],
+      }),
+      prisma.mylist.create({
+        data: {
+          id: "m1",
+          title: "Mylist 1",
+          holderId: "u1",
+          isLikeList: true,
+          shareRange: MylistShareRange.PUBLIC,
+        },
+      }),
+    ]);
+
+    const actual = await get(prisma, { userId: "u1", authUserId: null });
+    expect(actual).toStrictEqual(
+      ok(
+        expect.objectContaining({
+          id: "m1",
+          title: "Mylist 1",
+          holderId: "u1",
+        } satisfies Partial<Ok<Awaited<ReturnType<typeof get>>>["data"]>)
+      )
+    );
+  });
+});
--- src/resolvers/User/likes.ts
+++ src/resolvers/User/likes.ts
@@ -1,22 +1,34 @@
-import { MylistShareRange } from "@prisma/client";
-import { GraphQLError } from "graphql";
+import { Mylist, MylistShareRange } from "@prisma/client";
 
+import { err, ok, Result } from "../../utils/Result.js";
 import { UserResolvers } from "../graphql.js";
 import { ResolverDeps } from "../index.js";
 import { MylistModel } from "../Mylist/model.js";
 
+export const get = async (
+  prisma: ResolverDeps["prisma"],
+  { userId, authUserId }: { userId: string; authUserId: string | null }
+): Promise<Result<"NO_LIKELIST" | "PRIVATE_NOT_HOLDER", Mylist>> => {
+  const mylist = await prisma.mylist.findFirst({ where: { holderId: userId, isLikeList: true } });
+
+  if (!mylist) return err("NO_LIKELIST");
+  if (mylist.shareRange !== MylistShareRange.PUBLIC && mylist.holderId !== authUserId) err("PRIVATE_NOT_HOLDER");
+
+  return ok(mylist);
+};
+
 export const resolveUserLikes = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
-  (async ({ id: userId }, _args, { userId: authUserId }) => {
-    const mylist = await prisma.mylist
-      .findFirstOrThrow({
-        where: { holderId: userId, isLikeList: true },
-      })
-      .catch(() => {
-        throw new GraphQLError(`User "${userId}" likes list not found`);
-      });
+  (async ({ id: userId }, _args, { user }) => {
+    const result = await get(prisma, { userId, authUserId: user?.id || null });
 
-    // 現状ではnullを返すが何らかのエラー型のunionにしても良い気がする
-    if (mylist.shareRange !== MylistShareRange.PUBLIC && authUserId !== userId) return null;
+    if (result.status === "error")
+      switch (result.error) {
+        case "NO_LIKELIST":
+          // TODO: logging
+          return null;
+        case "PRIVATE_NOT_HOLDER":
+          return null;
+      }
 
-    return new MylistModel(mylist);
+    return new MylistModel(result.data);
   }) satisfies UserResolvers["likes"];
