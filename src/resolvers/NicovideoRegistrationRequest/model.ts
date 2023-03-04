bc62475069b6fc77fa8bd8d7ccac3a4d8517e0fd
SnO₂WMaN <me@sno2wman.net>
2023-03-04 15:38:28 +0900

    feat: NicovideoRegistrationRequestConnectionを実装 (#383)
    * feat: NicovideoRegistrationRequestConnectionを実装
    
    Fixes #382
    
    * fix test

--- src/resolvers/NicovideoRegistrationRequest/model.ts
+++ src/resolvers/NicovideoRegistrationRequest/model.ts
@@ -3,6 +3,10 @@ import { NicovideoRegistrationRequest } from "@prisma/client";
 export class NicovideoRegistrationRequestModel {
   constructor(private readonly request: NicovideoRegistrationRequest) {}
 
+  public static fromPrisma(conn: NicovideoRegistrationRequest) {
+    return new NicovideoRegistrationRequestModel(conn);
+  }
+
   get dbId() {
     return this.request.id;
   }
--- src/resolvers/Query/findNicovideoRegistrationRequests/findNicovideoRegistrationRequests.ts
+++ src/resolvers/Query/findNicovideoRegistrationRequests/findNicovideoRegistrationRequests.ts
@@ -1,19 +1,44 @@
+import { findManyCursorConnection } from "@devoxa/prisma-relay-cursor-connection";
+import { GraphQLError } from "graphql";
+import z from "zod";
+
+import { cursorOptions } from "../../connection.js";
 import { QueryResolvers } from "../../graphql.js";
 import { ResolverDeps } from "../../index.js";
-import { NicovideoRegistrationRequestModel } from "../../NicovideoRegistrationRequest/model.js";
-import { parseSortOrder } from "../../parseSortOrder.js";
+import { NicovideoRegistrationRequestConnectionModel } from "../../NicovideoRegistrationRequestConnection/model.js";
+import { parseSortOrder as parseOrderBy } from "../../parseSortOrder.js";
 
-export const findNicovideoRegistrationRequests = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
-  (async (_, { input }) => {
-    const semitags = await prisma.nicovideoRegistrationRequest.findMany({
-      take: input.limit,
-      skip: input.skip,
-      orderBy: {
-        createdAt: parseSortOrder(input.order?.createdAt),
-      },
-      where: {
-        isChecked: input.checked?.valueOf(),
-      },
-    });
-    return { nodes: semitags.map((t) => new NicovideoRegistrationRequestModel(t)) };
+export const findNicovideoRegistrationRequests = ({ prisma, logger }: Pick<ResolverDeps, "prisma" | "logger">) =>
+  (async (_, { orderBy, checked, ...unparsedConnectionArgs }, { user: ctxUser }, info) => {
+    const connectionArgs = z
+      .union([
+        z.object({
+          first: z.number(),
+          after: z.string().optional(),
+        }),
+        z.object({
+          last: z.number(),
+          before: z.string().optional(),
+        }),
+      ])
+      .safeParse(unparsedConnectionArgs);
+    if (!connectionArgs.success) {
+      logger.error(
+        { path: info.path, args: { orderBy, ...unparsedConnectionArgs }, userId: ctxUser?.id },
+        "Wrong args"
+      );
+      throw new GraphQLError("Wrong args");
+    }
+
+    return findManyCursorConnection(
+      (args) =>
+        prisma.nicovideoRegistrationRequest.findMany({
+          where: { isChecked: checked?.valueOf() },
+          orderBy: { createdAt: parseOrderBy(orderBy.createdAt) },
+          ...args,
+        }),
+      () => prisma.nicovideoRegistrationRequest.count({ where: { isChecked: checked?.valueOf() } }),
+      connectionArgs.data,
+      { resolveInfo: info, ...cursorOptions }
+    ).then((c) => NicovideoRegistrationRequestConnectionModel.fromPrisma(c));
   }) satisfies QueryResolvers["findNicovideoRegistrationRequests"];
