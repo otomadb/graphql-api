26bfc10f0d91e3be399300a4c1a620eefd1cfe4e
SnO2WMaN <me@sno2wman.net>
2023-03-16 00:26:15 +0900

    fix

--- src/resolvers/Mylist/registrations/resolver.ts
+++ src/resolvers/Mylist/registrations/resolver.ts
@@ -4,9 +4,9 @@ import z from "zod";
 
 import { cursorOptions } from "../../connection.js";
 import { MylistResolvers } from "../../graphql.js";
-import { ResolverDeps } from "../../index.js";
 import { MylistRegistrationConnectionModel } from "../../MylistRegistrationConnection/model.js";
 import { parseSortOrder as parseOrderBy } from "../../parseSortOrder.js";
+import { ResolverDeps } from "../../types.js";
 
 export const resolverMylistRegistrations = ({ prisma, logger }: Pick<ResolverDeps, "prisma" | "logger">) =>
   (async ({ id: mylistId }, { orderBy, ...unparsedConnectionArgs }, { user: ctxUser }, info) => {
