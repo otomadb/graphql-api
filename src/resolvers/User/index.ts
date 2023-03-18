08ad18e167b3b87f715ab2489ec3cea015deddc2
SnO₂WMaN <me@sno2wman.net>
2023-03-19 08:55:18 +0900

    feat: ニコニコ動画のリクエスト一覧をユーザから取得可能に (#417)
    * feat: ニコニコ動画のリクエスト一覧をユーザから取得可能に
    
    Fixes #415
    
    * fix type

--- src/resolvers/User/index.ts
+++ src/resolvers/User/index.ts
@@ -6,11 +6,13 @@ import { MylistModel } from "../Mylist/model.js";
 import { ResolverDeps } from "../types.js";
 import { resolverUserLikes } from "./likes/resolver.js";
 import { resolverUserMylists } from "./mylists/resolver.js";
+import { resolverUserNicovideoRegistrationRequests } from "./nicovideoRegistrationRequests/resolver.js";
 
 export const resolveUser = ({ prisma, logger }: Pick<ResolverDeps, "prisma" | "logger">) =>
   ({
     id: ({ id }): string => buildGqlId("User", id),
     likes: resolverUserLikes({ prisma }),
+    nicovideoRegistrationRequests: resolverUserNicovideoRegistrationRequests({ prisma, logger }),
 
     mylist: async ({ id: userId }, { id: gqlMylistId }, { user: ctxUser }) => {
       const mylist = await prisma.mylist.findFirst({ where: { id: parseGqlID("Mylist", gqlMylistId) } });
