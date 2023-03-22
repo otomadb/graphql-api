7f4fedb3765998136df9a4bb51e1cc679b559842
SnO₂WMaN <me@sno2wman.net>
2023-03-23 06:19:54 +0900

    feat: `Query.findSemitag`を名前順で取得するように変更 (#425)

--- src/resolvers/parseSortOrder.ts
+++ src/resolvers/parseSortOrder.ts
@@ -1,5 +1,6 @@
 import { Prisma } from "@prisma/client";
 
+import { err, ok, Result } from "../utils/Result.js";
 import { InputMaybe, SortOrder } from "./graphql.js";
 
 export const parseSortOrder = (v: InputMaybe<SortOrder> | undefined): Prisma.SortOrder | undefined => {
@@ -9,5 +10,23 @@ export const parseSortOrder = (v: InputMaybe<SortOrder> | undefined): Prisma.Sor
       return Prisma.SortOrder.asc;
     case SortOrder.Desc:
       return Prisma.SortOrder.desc;
+  }
+};
+
+export const parseOrderBy = <T extends string>(
+  orderBy: { [key in T]?: InputMaybe<SortOrder> },
+  def: [T, Prisma.SortOrder]
+): Result<{ message: "DUP" }, { [key in T]: Prisma.SortOrder }> => {
+  const entities = Object.entries(orderBy);
+
+  if (entities.length === 0) return ok({ [def[0]]: def[1] } as { [key in T]: Prisma.SortOrder });
+  if (2 <= entities.length) return err({ message: "DUP" });
+
+  const [key, val] = entities[0] as [T, SortOrder];
+  switch (val) {
+    case SortOrder.Asc:
+      return ok({ [key]: Prisma.SortOrder.asc } as { [key in T]: Prisma.SortOrder });
+    case SortOrder.Desc:
+      return ok({ [key]: Prisma.SortOrder.desc } as { [key in T]: Prisma.SortOrder });
   }
 };
