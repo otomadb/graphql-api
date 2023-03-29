e5d42618f0dbb7ac9e72f51b63c653569d5b6d0f
SnO₂WMaN <me@sno2wman.net>
2023-03-29 16:32:37 +0900

    feat: `Semitag.suggestTags`でこのタグに解決可能かどうかのプロパティを生やす (#470)
    * feat: `resolveSemitag`と`rejectSemitag`の引数と返り値を変更
    
    * feat: `Semitag.suggestTags`が提案したタグに解決可能かどうかを入れる
    
    Fixes #468

---
+++ src/resolvers/SemitagSuggestTagsItem/model.ts
@@ -0,0 +1,25 @@
+export class SemitagSuggestTagsItemModel {
+  private constructor(
+    private readonly entity: {
+      nameId: string;
+      tagId: string;
+      semitagId: string;
+    }
+  ) {}
+
+  static make(entity: { nameId: string; tagId: string; semitagId: string }) {
+    return new SemitagSuggestTagsItemModel(entity);
+  }
+
+  get tagId() {
+    return this.entity.tagId;
+  }
+
+  get nameId() {
+    return this.entity.nameId;
+  }
+
+  get semitagId() {
+    return this.entity.semitagId;
+  }
+}
