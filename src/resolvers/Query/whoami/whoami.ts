b195b29152a34b733fad52cdec3f19a758e8e192
SnO2WMaN <me@sno2wman.net>
2023-04-05 16:31:32 +0900

    feat: `Query.whoami`を必ず存在するように

--- src/resolvers/Query/whoami/whoami.ts
+++
@@ -1,9 +0,0 @@
-import { QueryResolvers } from "../../graphql.js";
-import { ResolverDeps } from "../../types.js";
-import { UserModel } from "../../User/model.js";
-
-export const whoami = ({ auth0Management }: Pick<ResolverDeps, "auth0Management">) =>
-  (async (_parent, _args, { currentUser }) => {
-    const user = await auth0Management.getUser({ id: currentUser.id });
-    return UserModel.fromAuth0User(user);
-  }) satisfies QueryResolvers["whoami"];
