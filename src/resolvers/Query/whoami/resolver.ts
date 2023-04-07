77e429f625d210a81b237b0998800e134aa21f99
SnO2WMaN <me@sno2wman.net>
2023-04-07 19:19:11 +0900

    feat: `Query.viewer`の取得時に自動でUserをDB側に作成する
    Fixes #504

--- src/resolvers/Query/whoami/resolver.ts
+++ src/resolvers/Query/whoami/resolver.ts
@@ -4,13 +4,20 @@ import { QueryResolvers } from "../../graphql.js";
 import { ResolverDeps } from "../../types.js";
 import { UserModel } from "../../User/model.js";
 
-export const resolverWhoami = ({ auth0Management, logger }: Pick<ResolverDeps, "auth0Management" | "logger">) =>
+export const resolverWhoami = ({
+  prisma,
+  auth0Management,
+  logger,
+}: Pick<ResolverDeps, "prisma" | "auth0Management" | "logger">) =>
   (async (_parent, _args, { currentUser }, info) => {
     try {
       const user = await auth0Management.getUser({ id: currentUser.id });
+
+      await prisma.user.upsert({ where: { id: currentUser.id }, update: {}, create: { id: currentUser.id } });
+
       return UserModel.fromAuth0User(user);
     } catch (error) {
-      logger.error({ error, path: info.path }, "Internal server error");
+      logger.error({ error, path: info.path, currentUser }, "Internal server error");
       throw new GraphQLError("Internal server error");
     }
   }) satisfies QueryResolvers["whoami"];
