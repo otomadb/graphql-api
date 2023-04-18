7ddfa524e4cf9edf21f1c4de694f4dc840ad873a
SnO₂WMaN <me@sno2wman.net>
2023-04-18 14:53:08 +0900

    feat: 未チェックのニコニコ動画の登録申請取得の実装 (#565)
    Fixes #564

--- src/resolvers/parseSortOrder.ts
+++ src/resolvers/parseSortOrder.ts
@@ -30,3 +30,21 @@ export const parseOrderBy = <T extends string>(
       return ok({ [key]: Prisma.SortOrder.desc } as { [key in T]: Prisma.SortOrder });
   }
 };
+
+export const parseOrderBy2 = <T extends string>(orderBy: { [key in T]?: InputMaybe<SortOrder> }): Result<
+  { message: "DUP" },
+  { [key in T]: Prisma.SortOrder }
+> => {
+  const entities = Object.entries(orderBy);
+
+  if (entities.length === 0) return ok({} as { [key in T]: Prisma.SortOrder });
+  if (2 <= entities.length) return err({ message: "DUP" });
+
+  const [key, val] = entities[0] as [T, SortOrder];
+  switch (val) {
+    case SortOrder.Asc:
+      return ok({ [key]: Prisma.SortOrder.asc } as { [key in T]: Prisma.SortOrder });
+    case SortOrder.Desc:
+      return ok({ [key]: Prisma.SortOrder.desc } as { [key in T]: Prisma.SortOrder });
+  }
+};
