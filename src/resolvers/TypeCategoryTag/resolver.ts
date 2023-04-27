1632c0b5c99cc36b91e9d37ac3c7d1530325b973
SnO2WMaN <me@sno2wman.net>
2023-04-27 19:48:35 +0900

    feat: 全てのカテゴリータグタイプを取得するクエリ
    Fixes #585

---
+++ src/resolvers/TypeCategoryTag/resolver.ts
@@ -0,0 +1,28 @@
+import { Resolvers, TypeCategoryTagType } from "../graphql.js";
+import { TagModel } from "../Tag/model.js";
+import { ResolverDeps } from "../types.js";
+
+export const resolverTypeCategoryTag = ({ prisma }: Pick<ResolverDeps, "prisma">) =>
+  ({
+    tag: ({ tagId }) => prisma.tag.findUniqueOrThrow({ where: { id: tagId } }).then((t) => TagModel.fromPrisma(t)),
+    type({ type }) {
+      switch (type) {
+        case "CHARACTER":
+          return TypeCategoryTagType.Character;
+        case "COPYRIGHT":
+          return TypeCategoryTagType.Copyright;
+        case "EVENT":
+          return TypeCategoryTagType.Event;
+        case "MUSIC":
+          return TypeCategoryTagType.Music;
+        case "PHRASE":
+          return TypeCategoryTagType.Phrase;
+        case "SERIES":
+          return TypeCategoryTagType.Series;
+        case "STYLE":
+          return TypeCategoryTagType.Style;
+        case "TACTICS":
+          return TypeCategoryTagType.Tactics;
+      }
+    },
+  } satisfies Resolvers["TypeCategoryTag"]);
