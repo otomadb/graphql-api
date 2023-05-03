62b69f835f41758d3dc51ac2790768943aeabb7a
SnO2WMaN <me@sno2wman.net>
2023-05-03 22:09:08 +0900

    fix: Auth0 Management APIのスコープを修正

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ const auth0Management = new ManagementClient(
         domain: process.env.AUTH0_DOMAIN,
         clientId: process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        scope: "read:users",
+        scope: ["read:users", "update:users", "read:roles"].join(" "),
       }
 );
 
