7512bb4f5bb51dcbf2289c49b2cc5e94e9bde8d0
SnO2WMaN <me@sno2wman.net>
2023-05-17 21:19:18 +0900

    fx

--- src/resolvers/Video/index.ts
+++ src/resolvers/Video/index.ts
@@ -62,11 +62,6 @@ export const resolveVideo = ({ prisma, neo4j, logger }: Pick<ResolverDeps, "pris
         .findMany({ where: { videoId } })
         .then((ss) => ss.map((s) => SoundcloudVideoSourceDTO.fromPrisma(s))),
 
-    soundcloudSources: async ({ id: videoId }) =>
-      prisma.soundcloudVideoSource
-        .findMany({ where: { videoId } })
-        .then((ss) => ss.map((s) => SoundcloudVideoSourceDTO.fromPrisma(s))),
-
     semitags: ({ id: videoId }, { checked }) =>
       prisma.semitag
         .findMany({ where: { videoId, isChecked: checked?.valueOf() } })
