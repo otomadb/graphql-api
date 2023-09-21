4fb9ba941b5a8a21f55f5296212d47e6584c5435
SnO2WMaN <me@sno2wman.net>
2023-09-22 08:27:18 +0900

    testfix

--- src/BilibiliMADSource/BilibiliMADSource.resolver.test.ts
+++ src/BilibiliMADSource/BilibiliMADSource.resolver.test.ts
@@ -1,21 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { GraphQLResolveInfo } from "graphql";
 import { afterAll, beforeAll, beforeEach, describe, expect, test } from "vitest";
 
-import { ResolverDeps } from "../resolvers/types.js";
+import { Context, ResolverDeps } from "../resolvers/types.js";
 import { cleanPrisma } from "../test/cleanPrisma.js";
 import { BilibiliMADSourceDTO } from "./BilibiliMADSource.dto.js";
-import mkresolver from "./BilibiliMADSource.resolver.js";
+import { resolverBilibiliMADSource } from "./BilibiliMADSource.resolver.js";
 
 describe("resolverBilibiliMADSource", () => {
   let prisma: ResolverDeps["prisma"];
 
-  let resolver: ReturnType<typeof mkresolver>;
+  let resolver: ReturnType<typeof resolverBilibiliMADSource>;
 
   beforeAll(async () => {
     prisma = new PrismaClient();
     await prisma.$connect();
 
-    resolver = mkresolver({ prisma });
+    resolver = resolverBilibiliMADSource({ prisma });
   });
 
   beforeEach(async () => {
@@ -27,31 +28,50 @@ describe("resolverBilibiliMADSource", () => {
   });
 
   test("sourceId", () => {
-    expect(resolver.sourceId({ sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO)).toBe("BV1xx411c7mu");
+    expect(
+      resolver.sourceId(
+        { sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO,
+        {},
+        {} as Context,
+        {} as GraphQLResolveInfo,
+      ),
+    ).toBe("BV1xx411c7mu");
   });
 
   test("url", () => {
-    expect(resolver.url({ sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO)).toBe(
-      "https://www.bilibili.com/video/BV1xx411c7mu",
-    );
+    expect(
+      resolver.url({ sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO, {}, {} as Context, {} as GraphQLResolveInfo),
+    ).toBe("https://www.bilibili.com/video/BV1xx411c7mu");
   });
 
   test("embedUrl", () => {
-    expect(resolver.embedUrl({ sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO)).toBe(
-      "https://player.bilibili.com/player.html?bvid=BV1xx411c7mu",
-    );
+    expect(
+      resolver.embedUrl(
+        { sourceId: "BV1xx411c7mu" } as BilibiliMADSourceDTO,
+        {},
+        {} as Context,
+        {} as GraphQLResolveInfo,
+      ),
+    ).toBe("https://player.bilibili.com/player.html?bvid=BV1xx411c7mu");
   });
 
   describe("video", () => {
     test("存在する", async () => {
       await prisma.video.create({ data: { id: "1" } });
 
-      const actual = await resolver.video({ videoId: "1" } as BilibiliMADSourceDTO);
+      const actual = await resolver.video(
+        { videoId: "1" } as BilibiliMADSourceDTO,
+        {},
+        {} as Context,
+        {} as GraphQLResolveInfo,
+      );
       expect(actual.id).toBe("1");
     });
 
     test("存在しない", async () => {
-      await expect(resolver.video({ videoId: "1" } as BilibiliMADSourceDTO)).rejects.toThrowError();
+      await expect(
+        resolver.video({ videoId: "1" } as BilibiliMADSourceDTO, {}, {} as Context, {} as GraphQLResolveInfo),
+      ).rejects.toThrowError();
     });
   });
 });
