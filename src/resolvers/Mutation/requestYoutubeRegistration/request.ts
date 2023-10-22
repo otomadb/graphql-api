aa931c41b2e2966d8c6415dc4c66afef72589c50
SnO₂WMaN <me@sno2wman.net>
2023-10-23 07:57:46 +0900

    追加リクエストの追加時のEventのtypeが`ACCEPT`になっていた問題を修正 (#832)

--- src/resolvers/Mutation/requestYoutubeRegistration/request.ts
+++ src/resolvers/Mutation/requestYoutubeRegistration/request.ts
@@ -66,7 +66,7 @@ export const requestRegistration = async (
             })),
           },
         },
-        events: { create: { userId, type: "ACCEPT" } },
+        events: { create: { userId, type: "REQUEST" } },
       },
     });
 
