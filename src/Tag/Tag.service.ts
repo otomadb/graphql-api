5074fa573ad3521466285e9b4371f0e229b521c0
SnO₂WMaN <me@sno2wman.net>
2023-11-29 08:29:04 +0900

    `Tag.taggedVideosByOffset`の実装 (#967)
    * Nicochuuからデータ取得
    
    * fx
    
    * `Tag.taggedVideosByOffset`の実装
    
    Fixes #963
    
    * add totalcount

--- src/Tag/Tag.service.ts
+++ src/Tag/Tag.service.ts
@@ -1,9 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 
+import { VideoTagDTO } from "../Video/dto.js";
+
 export const mkTagService = ({ prisma }: { prisma: PrismaClient }) => {
   return {
     countAll() {
       return prisma.tag.count();
+    },
+    async taggedVideosByOffset(
+      tagId: string,
+      { offset, take, orderBy }: { offset: number; take: number; orderBy: { createdAt?: "desc" | "asc" } },
+    ) {
+      const [count, nodes] = await prisma.$transaction([
+        prisma.videoTag.count({ where: { tagId } }),
+        prisma.videoTag.findMany({ where: { tagId }, orderBy, skip: offset, take }),
+      ]);
+      return {
+        hasMore: offset + take < count,
+        totalCount: count,
+        nodes: nodes.map((v) => new VideoTagDTO(v)),
+      };
     },
   };
 };
