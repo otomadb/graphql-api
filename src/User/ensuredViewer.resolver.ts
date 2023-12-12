86ce9fc6ad904da9efb34c8c19e6dab6de3d88bb
SnO₂WMaN <me@sno2wman.net>
2023-12-13 02:51:55 +0900

    `Query.ensuredViewer` (#1102)
    * `User.mylist`を修正
    
    * `Query.ensuredViewer`
    
    Fixes #1090
    
    * fx

--- src/User/ensuredViewer.resolver.ts
+++ src/User/ensuredViewer.resolver.ts
@@ -2,14 +2,12 @@ import { GraphQLError } from "graphql";
 
 import { MkQueryResolver } from "../utils/MkResolver.js";
 
-export const mkViewerResolver: MkQueryResolver<"viewer", "userService" | "logger"> =
-  ({ userService, logger }) =>
+export const mkViewerResolver: MkQueryResolver<"viewer", "userService"> =
+  ({ userService }) =>
   async (_parent, _args, { currentUser }) => {
-    if (!currentUser) return null;
     try {
       return userService.getById(currentUser.id);
     } catch (e) {
-      logger.error(e);
       throw new GraphQLError("Internal server error");
     }
   };
