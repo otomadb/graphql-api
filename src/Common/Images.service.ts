7d85e7cf3dc87e18d47246fae040d75c6ea76c56
SnO2WMaN <me@sno2wman.net>
2023-12-19 02:23:04 +0900

    fix graphql-api

--- src/Common/Images.service.ts
+++ src/Common/Images.service.ts
@@ -1,83 +1,18 @@
-import { FetchExternalSourceThumbnailScale, VideoThumbnailScale } from "../resolvers/graphql.js";
+import { PromiseClient } from "@connectrpc/connect";
 
-export class ImagesService {
-  private constructor(private baseUrl: string) {}
+import { ProxyService } from "../protobuf/ixgyohn/proxy/v2/proxy_connect.js";
 
-  public static make({ env: { baseUrl } }: { env: { baseUrl: string } }) {
-    return new ImagesService(baseUrl);
-  }
+export const mkImageService = ({ i }: { i: PromiseClient<typeof ProxyService> }) => {
+  return {
+    proxyThis(imgUrl: string, scale: "LARGE" | "OGP"): Promise<string> {
+      switch (scale) {
+        case "LARGE":
+          return i.proxyUrl({ url: imgUrl, width: 700, height: 400 }).then((r) => r.proxiedUrl);
+        case "OGP":
+          return i.proxyUrl({ url: imgUrl, width: 700, height: 400 }).then((r) => r.proxiedUrl);
+      }
+    },
+  };
+};
 
-  public originalYoutube(vid: string) {
-    const url = new URL(`original/youtube/${vid}`, this.baseUrl);
-    url.searchParams.set("size", "960x720");
-    return url.toString();
-  }
-
-  public originalNicovideo(vid: string) {
-    const url = new URL(`original/nicovideo/${vid}`, this.baseUrl);
-    url.searchParams.set("size", "960x720");
-    return url.toString();
-  }
-
-  public originalBilibili(vid: string, size: FetchExternalSourceThumbnailScale) {
-    const url = new URL(`original/bilibili/${vid}`, this.baseUrl);
-    switch (size) {
-      case FetchExternalSourceThumbnailScale.Large:
-        url.searchParams.set("scale", "large");
-        break;
-      case FetchExternalSourceThumbnailScale.Ogp:
-        url.searchParams.set("scale", "ogp");
-        break;
-    }
-    return url.toString();
-  }
-
-  public getOriginalSoundcloudUrl(soundcloudUrl: string, size: FetchExternalSourceThumbnailScale) {
-    const url = new URL(`original/soundcloud`, this.baseUrl);
-    url.searchParams.set("url", soundcloudUrl);
-    switch (size) {
-      case FetchExternalSourceThumbnailScale.Large:
-        url.searchParams.set("scale", "large");
-        break;
-      case FetchExternalSourceThumbnailScale.Ogp:
-        url.searchParams.set("scale", "ogp");
-        break;
-    }
-    return url.toString();
-  }
-
-  public thumbnailPrimary(serial: number, size: VideoThumbnailScale) {
-    const url = new URL(`/mads/${serial}/primary`, this.baseUrl);
-    switch (size) {
-      case VideoThumbnailScale.Large:
-        url.searchParams.set("scale", "large");
-        break;
-      case VideoThumbnailScale.Ogp:
-        url.searchParams.set("scale", "ogp");
-        break;
-    }
-    return url.toString();
-  }
-
-  public prepareDummy(imageUrl: string | null): string {
-    if (imageUrl) return imageUrl;
-
-    // TODO: 早く直す
-    return "https://img.youtube.com/vi/0/0.jpg";
-  }
-
-  public proxyThis(imgUrl: string, scale: "LARGE" | "OGP"): string {
-    const url = new URL(`proxy`, this.baseUrl);
-    url.searchParams.set("url", imgUrl);
-    switch (scale) {
-      case "LARGE":
-        url.searchParams.set("scale", "large");
-        break;
-      case "OGP":
-        url.searchParams.set("scale", "ogp");
-        break;
-    }
-
-    return url.toString();
-  }
-}
+export type ImagesService = ReturnType<typeof mkImageService>;
